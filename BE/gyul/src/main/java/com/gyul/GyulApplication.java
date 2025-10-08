df499c8c398ebc3b9917f9262d0cf5db4b8d9954
sehyun <germs1020@gmail.com>
2025-10-08 16:16:13 +0900

    20251008 init BE files

---
+++ BE/gyul/src/main/java/com/gyul/GyulApplication.java
@@ -0,0 +1,13 @@
+package com.gyul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GyulApplication.class, args);
+	}
+
+}
---
+++ BE/gyul/src/test/java/com/gyul/GyulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GyulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
